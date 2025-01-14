2fbb21634c6927b3427fdfa05c3620fff9cfc0e8
cvecina <cvv@motivales.com>
2025-01-14 19:55:26 +0100

    agregar ideas para funcionalidades de seguimiento de entrenamiento y dieta

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
     <HolaMundo></HolaMundo>
+    Quiero hacer un par de cosas en este proyecto.
+
+    Quiero apuntar las rutinas de entrenamiento.
+    Poner los días que voy y los ejercicios que hago.
+    Poner los pesos que hago para cada ejercicio.
+    Poner las repeticiones que hago para cada ejercicio.
+    Poner las series que hago para cada ejercicio.
+
+    Quiero poder ver un resumen de los ejercicios que hice en la semana.
+    Quiero poder ver un resumen de los ejercicios que hice en el mes.
+    Quiero poder ver un resumen de los ejercicios que hice en el año.
+
+    Quiero poder ver un resumen de los pesos que levanté en la semana.
+    Quiero poder ver un resumen de los pesos que levanté en el mes.
+    Quiero poder ver un resumen de los pesos que levanté en el año.
+
+    Quiero tener una zona de dietas en la que pueda generar una dieta en base a mis necesidades.
+    Quiero poder ver un resumen de las calorías que consumí en la semana.
 </template>
